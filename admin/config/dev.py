f70359faaa886136db59d030710743a1b332ed18
zhangyuwei <zyw@tonnn.com>
2019-03-19 17:29:07 +0800

    init

--- admin/config/dev.py
+++ admin/config/dev.py
@@ -3,5 +3,5 @@ from . import BaseConfig
 class DevConfig(BaseConfig):
     DEBUG = True
     REDIS_URL = "redis://:@localhost:6379/0"
-    SQLALCHEMY_DATABASE_URI = 'postgresql://zhangyuwei:123456@10.0.2.108:5432/yeyou'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://zhangyuwei:123456@127.0.0.1:5432/mydb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
